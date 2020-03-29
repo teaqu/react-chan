504e810f282680d631194890c582bf519c1c284f
teaqu <teaqu@outlook.com>
2020-03-29 12:42:23 +0100

    test catalog action creators

--- src/catalog/__tests__/catalog-actions-test.tsx
+++ src/catalog/__tests__/catalog-actions-test.tsx
@@ -8,6 +8,7 @@ import { AnyAction } from 'redux';
 import { RootState } from 'src/shared/root-reducer';
 
 import * as actions from '../catalog-actions';
+import { Thread } from '../thread';
 
 import catalogResponse from './__mocks__/catalog-response.json';
 
@@ -16,6 +17,30 @@ const middlewares = [thunk];
 describe('catalog actions', () => {
   afterEach(() => {
     fetchMock.restore();
+  });
+
+  it('should create an action to request the catalog', () => {
+    const board = 'qa';
+    const expectedAction = {
+      payload: board,
+      type: actions.requestCatalog.type
+    };
+    expect(actions.requestCatalog(board)).toEqual(expectedAction);
+  });
+
+  it('should create an action to receive the catalog', () => {
+    const board = 'g';
+    const threads: Thread[] = catalogResponse[0].threads;
+    Date.now = jest.fn(() => 1111);
+    const expectedAction = {
+      payload: {
+        receivedAt: 1111,
+        board: board,
+        threads: threads
+      },
+      type: actions.receiveCatalog.type
+    };
+    expect(actions.receiveCatalog(board, threads)).toEqual(expectedAction);
   });
 
   it('should create an action to recive and request the catalog', async () => {
